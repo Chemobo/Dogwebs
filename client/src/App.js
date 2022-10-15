e44010bcdccf0367beaaa2555c7f926ae6c56f7b
Chemobo <jkchemobo@gmail.com>
2022-10-15 16:28:53 +0300

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import NavBar from './Components/NavBar';
+
 
 function App() {
+  const [user, setUser] = useState(null); 
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar user={user} setUser={setUser} />
+    <main>
+      {user?(
+        <>
+        <Switch>
+              <Route strict path="/">
+                <Home exact user={user}/>
+              </Route>                          
+            </Switch>
+        </>
+      ):(
+        <Switch>
+            <Route path="/signup">
+              <Signup setUser={setUser} />
+            </Route>
+            <Route path="/login">
+              <Login setUser={setUser} />
+            </Route>
+            <Route strict path="/">
+                <Home />
+            </Route>
+          </Switch>
+      )}
+    </main>
+    </>
+  
   );
 }
 
