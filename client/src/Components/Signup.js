1b8f4da30e3e15999209f5ac868b42613e35e636
Chemobo <jkchemobo@gmail.com>
2022-10-15 17:20:41 +0300

    creates signup

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,8 +1,97 @@
-import React from 'react'
+import {useState }from 'react';
 
 function Signup() {
+  const [username, setUsername] = useState("");
+  const [firstname, setFistName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        firstname,
+        lastname,
+        email,
+        password,
+        password_confirmation: passwordConfirmation,
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }
+
   return (
-    <div>Signup</div>
+    <div className="mainSection">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={ (e) => setUsername(e.target.value) }
+          required
+        />
+        <label htmlFor="firstname">First name</label>
+        <input
+          type="firstname"
+          id="firstname"
+          value={firstname}
+          onChange={(e) => setFistName(e.target.value)}
+          autoComplete="current-firstname"
+          required
+        />
+
+        <label htmlFor="lastname">Last name</label>
+        <input
+          type="lastname"
+          id="lastname"
+          value={lastname}
+          onChange={(e) => setLastName(e.target.value)}
+          autoComplete="current-lastname"
+          required
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          autoComplete="current-email"
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+          required
+        />
+        <label htmlFor="password">Password Confirmation</label>
+        <input
+          type="password"
+          id="password_confirmation"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+          autoComplete="current-password"
+          required
+        />
+      <button type="submit" className="btn-signup" >Sign Up</button>
+      </form>
+    </div>
   )
 }
 
